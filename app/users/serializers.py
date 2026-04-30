25e4d25682bb20c74be7a5c04757fc02c47a570a
onesch <vladimiravakov12345@gmail.com>
2026-04-30 18:25:58 +0400

    feat(users): add user password hashing to UserSerializer

--- app/users/serializers.py
+++ app/users/serializers.py
@@ -7,6 +7,16 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ["username", "password"]
+        extra_kwargs = {
+            "password": {"write_only": True}
+        }
+
+    def create(self, validated_data):
+        password = validated_data.pop("password")
+        user = User(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
 
 class PasswordSerializer(serializers.Serializer):
     old_password = serializers.CharField(required=True)
