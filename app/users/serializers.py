24a5dc048c6ac2ea2ea094088e66055ccd855c8a
onesch <vladimiravakov12345@gmail.com>
2026-04-30 18:20:40 +0400

    feat(users): add change_password action in UserViewSet

--- app/users/serializers.py
+++ app/users/serializers.py
@@ -6,4 +6,19 @@ from app.users.models import User
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ["username"]
+        fields = ["username", "password"]
+
+class PasswordSerializer(serializers.Serializer):
+    old_password = serializers.CharField(required=True)
+    new_password = serializers.CharField(required=True)
+
+    def save(self, **kwargs):
+        user = self.context["user"]
+
+        if not user.check_password(self.validated_data["old_password"]):
+            raise serializers.ValidationError("Wrong password")
+
+        user.set_password(self.validated_data["new_password"])
+        user.save()
+
+        return user
--- app/users/views.py
+++ app/users/views.py
@@ -1,11 +1,27 @@
 from rest_framework import viewsets
 from rest_framework.permissions import AllowAny
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from app.users.models import User
-from app.users.serializers import UserSerializer
+from app.users.serializers import UserSerializer, PasswordSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
+
+    @action(detail=True, methods=["post"])
+    def change_password(self, request, pk=None):
+        user = self.get_object()
+        serializer = PasswordSerializer(
+            data=request.data,
+            context={"user": user}
+        )
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"status": "password changed"})
+
+        return Response(serializer.errors, status=400)
