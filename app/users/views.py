143d30e3727e0dc6271411a56c396308a684c549
onesch <vladimiravakov12345@gmail.com>
2026-04-30 19:00:50 +0400

    feat(users): add get_users_without_password get action in UserViewSet

--- app/users/views.py
+++ app/users/views.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets
 from rest_framework.permissions import AllowAny
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from django.db.models.functions import Length
 
 from app.users.models import User
 from app.users.serializers import UserSerializer, PasswordSerializer
@@ -17,11 +18,18 @@ class UserViewSet(viewsets.ModelViewSet):
         user = self.get_object()
         serializer = PasswordSerializer(
             data=request.data,
-            context={"user": user}
+            context={"user": user},
         )
 
         if serializer.is_valid():
             serializer.save()
             return Response({"status": "password changed"})
-
         return Response(serializer.errors, status=400)
+
+    @action(detail=False, methods=["get"])
+    def get_users_without_password(self, request):
+        users = User.objects.annotate(
+            password_len=Length("password")
+            ).filter(password_len__lt=1)
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data)
