9321bc3dddd566307070be1011c1a19e392da359
onesch <vladimiravakov12345@gmail.com>
2026-05-06 17:41:50 +0400

    refactor(orders): improve models

--- app/orders/models.py
+++ app/orders/models.py
@@ -4,29 +4,7 @@ from app.products.models import Product
 from app.users.models import User
 
 
-class Order(models.Model):
-    date = models.DateTimeField(auto_now=True)
-
-    order_items = models.ManyToManyField(
-        Product,
-        through='OrderItem',
-        related_name='orders',
-    )
-
-    user = models.ForeignKey(
-        User,
-        null=True,
-        on_delete=models.PROTECT,
-        related_name='orders',
-    )
-
-
 class OrderItem(models.Model):
-    order = models.ForeignKey(
-        Order,
-        on_delete=models.CASCADE,
-        related_name='items',
-    )
     product = models.ForeignKey(
         Product,
         on_delete=models.CASCADE,
@@ -37,3 +15,26 @@ class OrderItem(models.Model):
     @property
     def total_price(self):
         return self.price_at_purchase * self.count
+
+
+class Order(models.Model):
+    date = models.DateTimeField(auto_now=True)
+
+    order_items = models.ForeignKey(
+        OrderItem,
+        null=True,
+        on_delete=models.PROTECT,
+        related_name='orders',
+    )
+
+    user = models.ForeignKey(
+        User,
+        null=True,
+        on_delete=models.PROTECT,
+        related_name='orders',
+    )
+
+    @property
+    def total_price(self):
+        for item in self.order_items:
+            ...
