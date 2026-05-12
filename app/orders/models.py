10903327464adfaf7e77396f4f2268e7ba5bb8f1
onesch <vladimiravakov12345@gmail.com>
2026-05-12 17:12:30 +0400

    refactor(orders): improve models

--- app/orders/models.py
+++ app/orders/models.py
@@ -1,31 +1,12 @@
 from django.db import models
+from django.db.models import F, Sum
 
 from app.products.models import Product
 from app.users.models import User
 
 
-class OrderItem(models.Model):
-    product = models.ForeignKey(
-        Product,
-        on_delete=models.CASCADE,
-    )
-    count = models.PositiveIntegerField(default=1)
-    price_at_purchase = models.IntegerField(null=True)
-
-    @property
-    def total_price(self):
-        return self.price_at_purchase * self.count
-
-
 class Order(models.Model):
     date = models.DateTimeField(auto_now=True)
-
-    order_items = models.ForeignKey(
-        OrderItem,
-        null=True,
-        on_delete=models.PROTECT,
-        related_name='orders',
-    )
 
     user = models.ForeignKey(
         User,
@@ -36,5 +17,26 @@ class Order(models.Model):
 
     @property
     def total_price(self):
-        for item in self.order_items:
-            ...
+        return self.items.aggregate(
+            total=Sum(F('price_at_purchase') * F('count'))
+        )['total'] or 0
+
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+    )
+    count = models.PositiveIntegerField(default=1)
+    price_at_purchase = models.IntegerField(null=True)
+
+    order = models.ForeignKey(
+            Order,
+            null=True,
+            on_delete=models.PROTECT,
+            related_name='items',
+        )
+
+    @property
+    def total_price(self):
+        return self.price_at_purchase * self.count
